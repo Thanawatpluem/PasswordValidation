8d92afaa0c6766d95da4c81f5f3d03d5afa5a4b7
p_lu4m <thanawat.boonc@ku.th>
2026-07-17 17:46:52 +0700

    Add files via upload

---
+++ PasswordValidator.java
@@ -0,0 +1,22 @@
+// ดูโจทย์ spec และวิธีทำใน README.md
+public class PasswordValidator {
+
+    static final int MIN_LEN = 8;
+    static final int MAX_LEN = 20;
+
+    static boolean validate(String pw) {
+        // TODO: implement ตาม spec ใน README.md (R1-R6)
+        if (pw == null) throw new IllegalArgumentException("pw must not be null");
+        if (pw.length() < MIN_LEN || pw.length() > MAX_LEN) return false;
+        boolean hasUpper = false, hasLower = false, hasDigit = false, hasWhitespace = false, hasSpecial = false;
+        for (int i = 0; i < pw.length(); i++) {
+            char c = pw.charAt(i);
+            if (Character.isUpperCase(c)) hasUpper = true;
+            if (Character.isLowerCase(c)) hasLower = true;
+            if (Character.isDigit(c)) hasDigit = true;
+            if (Character.isWhitespace(c)) hasWhitespace = true;
+            if (!Character.isLetterOrDigit(c)) hasSpecial = true;
+        }
+        return hasUpper && hasLower && hasDigit && !hasWhitespace && hasSpecial; 
+    }
+}
